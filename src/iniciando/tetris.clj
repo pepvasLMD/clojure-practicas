5630b330a8a0ce38b9da92e54ad834a7942c8039
Jose Eduardo Vasquez Gutierrez <pepvasLMD@gmail.com>
2020-06-13 22:04:19 -0500

    Sudoku

--- src/iniciando/tetris.clj
+++ src/iniciando/tetris.clj
@@ -1,5 +1,6 @@
 (ns iniciando.tetris
-  (:import [javax.swing JFrame JLabel JButton]
+  (
+    :import [javax.swing JFrame JLabel JButton]
            [java.awt.event WindowListener]))
 
 
@@ -130,86 +131,7 @@
 (permutations [1 2 3])
 
 
-(def b1 '[3 - - - - 5 - 1 -
-          - 7 - - - 6 - 3 -
-          1 - - - 9 - - - -
-          7 - 8 - - - - 9 -
-          9 - - 4 - 8 - - 2
-          - 6 - - - - 5 - 1
-          - - - - 4 - - - 6
-          - 4 - 7 - - - 2 -
-          - 2 - 6 - - - - 3])
 
-
-
-(defn prep [board]
-  (map #(partition 3 %)
-       (partition 9 board)))
-
-
-
-
-(defn print-board [board]
-  (let [row-sep (apply str (repeat 37 "-"))]
-    (println row-sep)
-    (dotimes [row (count board)]
-      (print "| ")
-      (doseq [subrow (nth board row)]
-        (doseq [cell (butlast subrow)]
-          (print (str cell " ")))
-        (print (str (last subrow) " | ")))
-      (println)
-      (when (zero? (mod (inc row) 3))
-        (println row-sep)))))
-
-
-
-(defn rows [board sz]
-  (partition sz board))
-
-
-(defn row-for [board index sz]
-  (nth (rows board sz) (/ index 9)))
-
-
-
-(defn column-for [board index sz]
-  (let [col (mod index sz)]
-    (map #(nth % col)
-         (rows board sz))))
-
-
-(defn subgrid-for [board i]
-  (let [rows (rows board 9)
-        sgcol (/ (mod i 9) 3)
-        sgrow (/ (/ i 9) 3)
-        grp-col (column-for (mapcat #(partition 3 %) rows) sgcol 3)
-        grp (take 3 (drop (* 3 (int sgrow)) grp-col))]
-    (flatten grp)))
-
-
-(defn numbers-present-for [board i]
-  (set
-    (concat (row-for board i 9)
-            (column-for board i 9)
-            (subgrid-for board i))))
-
-
-(-> b1 prep print-board)
-
-
-(row-for b1 1 9)
-
-
-(subgrid-for b1 1)
-
-
-(numbers-present-for b1 1)
-
-(numbers-present-for (assoc b1 1 8) 1)
-
-(set/difference #{1 2 3 4 5 6 7 8 9}
-                (numbers-present-for b1 1))
 
 
 ;(.show (javax.swing.JFrame.))
