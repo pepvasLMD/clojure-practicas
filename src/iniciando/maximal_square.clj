f7e1306d003f9e98202bf255ba6ba39694ac2a08
Eduardo <pepvasLMD@gmail.com>
2020-06-18 02:22:09 -0500

    Lines game

--- src/iniciando/maximal_square.clj
+++ src/iniciando/maximal_square.clj
@@ -1,20 +1,37 @@
 (ns iniciando.maximal-square)
 
-(def matrix [[1, 0, 1, 1, 1]
-              [1, 0, 1, 1, 1],
-               [1, 1, 1, 1, 1],
-                [1, 0, 0, 1, 0],
-                 [1, 0, 0, 1, 0]])
+(def matrix [["1","0","1","1","1"],
+             ["1","0","1","1","1"],
+             ["1","1","1","1","1"],
+             ["1","0","0","1","0"],
+             ["1","0","0","1","0"]])
 
 
-(defn get-cuadrado [matrix i f n]
-  (reduce (fn [x y] (conj x (take f (drop i y)))) [] (take n (drop i matrix))))
+(defn get-cuadrado [matrix xi yi xf yf]
+  (reduce (fn [x y] (conj x (take xf (drop xi y)))) [] (take yf (drop yi matrix))))
 
 (defn fill-posiciones? [matrix]
-  (= 0 (reduce (fn [x y] (+ x (count (filter #(= 0 %) y)))) 0 matrix)))
+  (reduce (fn [x y] (if (false? x) x (not (some #(= 0 (int %)) y)))) true matrix))
 
-(defn maximalSquare [matrix])
+(defn get-permutaciones [matrix]
+  (let [max-y (count matrix)
+         max-x (count (first matrix))]
+    (for [x (range max-x)
+          y (range max-y)
+          xf (range max-x)
+          yf (range max-y)]
+      [x y xf yf])))
 
+(defn area-matrix [matrix]
+  (let [num-rows (count matrix)
+         num-cols (count (first matrix))]
+    (* num-rows num-cols)))
 
-(get-cuadrado matrix 2 0)
-(fill-posiciones? (get-cuadrado matrix 0 2))
+(defn maximalSquare [matrix]
+  (let [perm (get-permutaciones matrix)
+        cuadrados (map (fn [x] (get-cuadrado matrix (x 0) (x 1) (x 2) (x 3))) perm)
+        areas (map area-matrix (filter fill-posiciones? cuadrados))]
+    (if (empty? areas) 0 (reduce max areas))))
+
+(maximalSquare[])
+(fill-posiciones?  matrix)
