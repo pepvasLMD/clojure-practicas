0f5cf758ef4916e90b0629007ab05e7c7442089c
Jose Eduardo Vasquez Gutierrez <pepvasLMD@gmail.com>
2020-06-17 20:56:25 -0500

    Lines Game

--- src/iniciando/maximal_square.clj
+++ src/iniciando/maximal_square.clj
@@ -7,8 +7,8 @@
                  [1, 0, 0, 1, 0]])
 
 
-(defn get-cuadrado [matrix i f]
-  (reduce (fn [x y] (conj x (take f (drop i y)))) [] (take f (drop i matrix))))
+(defn get-cuadrado [matrix i f n]
+  (reduce (fn [x y] (conj x (take f (drop i y)))) [] (take n (drop i matrix))))
 
 (defn fill-posiciones? [matrix]
   (= 0 (reduce (fn [x y] (+ x (count (filter #(= 0 %) y)))) 0 matrix)))
