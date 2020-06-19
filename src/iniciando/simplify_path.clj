9461661c6b8cb22e283474644609e7641383b17c
Eduardo <pepvasLMD@gmail.com>
2020-06-19 10:11:46 -0500

    Simplify path

---
+++ src/iniciando/simplify_path.clj
@@ -0,0 +1,17 @@
+(ns iniciando.simplify-path
+  (:require [clojure.string :as string]))
+
+(def path "/home/a/./x/../b//c/")
+
+(defn parent-directory [path]
+  (re-find (re-pattern "\\d+") "asdf321asfd") )
+
+
+(defn simplifyPath [path]
+  path)
+
+(simplifyPath path)
+
+(string/replace "The color is red" #"red" "blue")
+
+(parent-directory path)
