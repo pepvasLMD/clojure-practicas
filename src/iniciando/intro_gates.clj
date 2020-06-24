a3b87a46f0ac24a1175829dbe1cdcd9d28fa966b
Eduardo <pepvasLMD@gmail.com>
2020-06-24 03:59:33 -0500

    Intro gates

---
+++ src/iniciando/intro_gates.clj
@@ -0,0 +1,14 @@
+(ns iniciando.intro-gates)
+
+
+(defn addTwoDigits [n]
+  (loop [n n sum 0]
+    (if (= n 0)
+      sum
+      (recur (int (/ n 10)) (+ sum (mod n 10))))))
+
+(addTwoDigits 29)
+
+
+(defn largestNumber [n]
+  (- (int (Math/pow 10 n)) 1))
