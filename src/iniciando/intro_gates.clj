d64d16bb307ce9d6b0392e8e8f1ca35832c9f19c
Jose Eduardo Vasquez Gutierrez <pepvasLMD@gmail.com>
2020-06-24 23:55:27 -0500

    Intro Gates

--- src/iniciando/intro_gates.clj
+++ src/iniciando/intro_gates.clj
@@ -12,3 +12,58 @@
 
 (defn largestNumber [n]
   (- (int (Math/pow 10 n)) 1))
+
+
+
+(defn maxMultiple [divisor bound]
+  (loop [d divisor b bound]
+    (cond
+      (= b 1) b
+      (= (/ b d) (int (/ b d))) b
+      :else (recur d (dec b)))))
+
+
+(maxMultiple 3 10)
+
+
+(defn circleOfNumbers [n f]
+  (if (> (/ n 2) f) (+ f (/ n 2)) (- f (/ n 2))))
+
+(circleOfNumbers 10 8)
+
+(defn lateRide [n]
+  (let [h (int (/ n 60)) m (mod n 60)]
+    (+ (+ (int (/ h 10)) (mod h 10)) (+ (int (/ m 10)) (mod m 10)))))
+
+(lateRide 240)
+
+
+(defn phoneCall [min1 min2_10 min11 s]
+  (loop [s s min 0]
+    (cond
+      (<= s 0) min
+      (> min 9) (recur (- s min11) (inc min))
+      (> min 0) (recur (- s min2_10) (inc min))
+      :else (recur (- s min1) (inc min)))))
+
+(phoneCall 10 1 2 22)
+
+
+(defn chocan? [[y x] [y2 x2]]
+  (let [diferencia-y (max (- y y2) (- y2 y))
+        diferencia-x (max (- x x2) (- x2 x))]
+    (cond
+      (= diferencia-x diferencia-y) true
+      (or (= x x2) (= y y2))  true
+      (and (< x x2) (= y2 (+ y diferencia-x))) true
+      (and (< x2 x) (= y (+ y2 diferencia-x))) true
+      :else false)))
+
+
+(defn squaresUnderQueenAttack [n [q1 q2] queries]
+  (map (fn [[x y]] (or x y)) (map vector (map #(chocan? q1 %) queries) (map #(chocan? q2 %) queries))))
+
+
+(squaresUnderQueenAttack 5 [[1 1] [3 2]] [[1 1] [0 3] [0 4] [3 4] [2 0] [4 3] [4 0]])
+
+(chocan? [1 1] [3 2] )
