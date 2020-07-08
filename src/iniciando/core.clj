a81d0c73e1da186e645d528356f3eeb536c9c419
Eduardo <pepvasLMD@gmail.com>
2020-07-07 21:38:46 -0500

    main

--- src/iniciando/core.clj
+++ src/iniciando/core.clj
@@ -4,12 +4,3 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
-
-
-(defn fdasdfasdf
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
-
-(fdasdfasdf "asdf")
